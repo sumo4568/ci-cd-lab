d8d5b33ff4081678df184ad5fa6dc74df7512b7c
sumo4568 <92469642+sumo4568@users.noreply.github.com>
2023-11-15 17:45:58 +0100

    Initial commit

---
+++ src/test/java/com/example/cicdlab/CicdLabApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.cicdlab;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
